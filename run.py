62341a6686adf08089b7ef8fea679aef06d66679
Marketa Mia Tothova <21tothova@gmail.com>
2024-07-25 13:53:04 +0000

    Add a word dictionary to be used for word choices in the stories

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
+word_choice = {
+    'name' : ['Ella', 'Max', 'Whiskers', 'Daisy', 'Sparkle', 'Flash', 'Giggles', 'Bubbles', 'Snickers', 'Twinkle'],
+    'place': ['jungle', 'amusement park','spaghetti factory', 'rollercoaster', 'library', 'spaceship', 'giant pillow forth', 'candy store', 'zoo','aquarium'],
+    'liquid': ['river', 'pudding', 'chocolate', 'lemonade', 'milkshake', 'raibow', 'bubble', 'glitter', 'confetti', 'jelly'],
+    'body part': ['trunk', 'tail', 'paw', 'ear', 'nose', 'hoof', 'fin', 'wing', 'antenna', 'whisker'],
+    'thing 1': ['cereal bowl', 'treasure chest', 'ocean of pudding', 'circus tent', 'popcorn cloud', 'smoothie sea', 'cotton candy sky', 'giat pizza', 'pancake stack'],
+    'sound': ['fart noises', 'chicken clucks', 'robot beeps', 'disco beats', 'alien whispers', 'rap verses', 'karaoke songs', 'banjp solos', 'quacking ducks'],
+    'thing 2': ['playgrounds', 'trees', 'sandcastles', 'ball pits', 'snow globes', 'race tracks', 'toy boxes', 'candy lands', 'swimming pools'],
+    'action': ['dance moves', 'laugh', 'baloon animals', 'face paint', 'tickles', 'juggling', 'magic tricks', 'pranks', 'cartwheels'],
+    'food': ['marshmallows', 'glitter', 'bananas', 'rainbows', 'kites', 'ice cream cones', 'sprinkles', 'cupcakes']
+    
+}
 
 
 def start_game():
@@ -10,3 +22,38 @@ def start_game():
     print("1. The Funny Cow")
 
 start_game()
+
+
+def elephant_story():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    
+    
+def mouse_story():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+
+
+
+def cat_story():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+
+
+def cow_story():
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
