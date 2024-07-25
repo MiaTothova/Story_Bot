cfd4c60b82f4028daf92b2f00725b66ad88ffd38
Marketa Mia Tothova <21tothova@gmail.com>
2024-07-25 12:30:40 +0000

    Add start_game function. Add story line options

--- run.py
+++ run.py
@@ -1,3 +1,12 @@
 
 
-print("Welcome to story Bot")
+def start_game():
+    print("Welcome to Story Bot!\n")
+    print("I have 4 stories to choose from and YOU pick the words!")
+    print("Here they are: \n")
+    print("1. The Happy Elephant")
+    print("2. The Speedy Mouse")
+    print("3. The Curious Cat")
+    print("1. The Funny Cow")
+
+start_game()
